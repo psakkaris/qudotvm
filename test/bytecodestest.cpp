7a69b1308fa58e41bdac63e5aee55ee323c8edc3
periklis <psakkaris@gmail.com>
2019-04-20 22:31:15 -0700

    Added bytecode.cpp

---
+++ test/bytecodestest.cpp
@@ -0,0 +1,18 @@
+#include "gtest/gtest.h"
+#include "bytecodes.h"
+#include <iostream>
+
+TEST(BytecodesTest, testReadWrite) {
+    char memory[16];
+    
+    int index1 = qudot::writeInt(memory, 0, 2897);
+    int index2 = qudot::writeInt(memory, 4, -28);
+    int index3 = qudot::writeInt(memory, 8, 129347);
+    int index4 = qudot::writeInt(memory, 12, -527);
+    ASSERT_EQ(qudot::getInt(memory, 0), 2897);
+    ASSERT_EQ(qudot::getInt(memory, index1), -28);
+    ASSERT_EQ(qudot::getInt(memory, index2), 129347);
+    ASSERT_EQ(qudot::getInt(memory, index3), -527);
+
+    ASSERT_EQ(index4, 16);
+}
