fe7cd8c8104ae63b256e67b88b66a48a91119fde
periklis <psakkaris@gmail.com>
2019-05-06 09:17:56 -0700

    add parralel test for QuFrequency

--- test/qufrequencytest.cpp
+++ test/qufrequencytest.cpp
@@ -1,3 +1,4 @@
+#include <cstddef>
 #include <iostream>
 #include <string>
 
@@ -30,4 +31,23 @@ TEST(QuFrequencyTest, testSerialFrequency) {
     ASSERT_EQ(freq.getCount(result2), 1000);
     ASSERT_EQ(freq.getCount(result3), 3000);
     ASSERT_EQ(freq.getCount(no_result), 0);
+}
+
+TEST(QuFrequencyTest, testParallelFrequency){
+    QuFrequency freq(10000);
+
+    std::string result1 = "hello";
+    std::string result2 = "perry";
+    std::string result3 = "loof";
+
+    std::string no_result = "yoyo";
+
+    tbb::parallel_for(size_t(0), size_t(1000), [&](size_t i) { freq.addValue(result2); });
+    tbb::parallel_for(0, 6000, [&](size_t i) { freq.addValue(result1); });
+    tbb::parallel_for(0, 3000, [&](size_t i) { freq.addValue(result3); });
+
+    ASSERT_EQ(freq.getCount(result1), 6000);
+    ASSERT_EQ(freq.getCount(result2), 1000);
+    ASSERT_EQ(freq.getCount(result3), 3000);
+    ASSERT_EQ(freq.getCount(no_result), 0);    
 }
