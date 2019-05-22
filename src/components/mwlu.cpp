f85c76a342f1d32a2937bf84a61d732e6453d09c
periklis <psakkaris@gmail.com>
2019-05-22 10:50:48 -0700

    Implement QuWorld::addModN

--- src/components/mwlu.cpp
+++ src/components/mwlu.cpp
@@ -33,7 +33,11 @@ void ManyWorldsLogicUnit::addModN(QuMvN* qumvn, const int val, const int N, cons
 
 //**************** PRIVATE METHODS **********************8
 void ManyWorldsLogicUnit::addModN(QuWorld* quworld, const int val, const int N, const int start_q, const int end_q) {
-
+    int Na = N - val;
+    int mod_val = std::pow(2, end_q - start_q + 1) + val - N;
+    comparator.compare(quworld, Na, start_q, end_q);
+    full_adder.addClassicalInt(quworld, val, start_q, end_q, ONE);
+    full_adder.addClassicalInt(quworld, mod_val, start_q, end_q, ZERO);
 }
 
 }
